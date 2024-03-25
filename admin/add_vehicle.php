dd58808678a38b58504d92be18def2df258b0253
AdamAGalyoon <144166903+AdamAGalyoon@users.noreply.github.com>
2024-03-25 10:44:44 -0700

    Update add_vehicle.php

--- admin/add_vehicle.php
+++ admin/add_vehicle.php
@@ -46,7 +46,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Zippy Admin - Add Vehicle</title>
-    <!-- Bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
