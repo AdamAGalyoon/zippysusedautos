72efeff6b64b9d59b7e64e8ee6b702bee37f0e27
AdamAGalyoon <144166903+AdamAGalyoon@users.noreply.github.com>
2024-03-25 09:59:53 -0700

    Update index.php

--- index.php
+++ index.php
@@ -64,7 +64,6 @@ $vehicles = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <title>Zippy Used Autos</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <style>
-        /* Custom CSS styles */
         body {
             padding: 20px;
         }
